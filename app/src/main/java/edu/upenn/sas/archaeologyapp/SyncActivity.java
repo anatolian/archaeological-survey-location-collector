0f9c509848b9fac6def759c02ccc2b7514af4041
“eanvith” <eanvith@gmail.com>
2017-03-12 01:26:10 -0800

    Documentation for sync activity

--- app/src/main/java/edu/upenn/sas/archaeologyapp/SyncActivity.java
+++ app/src/main/java/edu/upenn/sas/archaeologyapp/SyncActivity.java
@@ -15,14 +15,36 @@ import net.gotev.uploadservice.UploadServiceBroadcastReceiver;
 
 import java.util.ArrayList;
 
+/**
+ * This activity is responsible for uploading all the records from the local database onto a server.
+ * Created by eanvith on 27/02/17.
+ */
+
 public class SyncActivity extends AppCompatActivity {
 
+    /**
+     * The button the user clicks to initiate the sync process
+     */
     Button syncButton;
 
-    int uploadIndex, totalItems;
-
+    /**
+     * A list of records populated from the local database, that need to be uploaded onto the server
+     */
     ArrayList<DataEntryElement> elementsToUpload;
 
+    /**
+     * The index of the element currently being uploaded in elementsToUpload list
+     */
+    int uploadIndex;
+
+    /**
+     * Total number of items contained in elementsToUpload list
+     */
+    int totalItems;
+
+    /**
+     * A database helper class object that enables fetching of records from the local database
+     */
     DataBaseHandler dataBaseHandler;
 
     @Override
@@ -30,26 +52,31 @@ public class SyncActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync);
 
+        // Initialise the database helper class object, and read in the records from the local databse
         dataBaseHandler = new DataBaseHandler(this);
-
         elementsToUpload = dataBaseHandler.getRows();
         totalItems = elementsToUpload.size();
         uploadIndex = 0;
 
+        // Attach a click listener to the sync button, and trigger the sync process on click of the button
         syncButton = (Button) findViewById(R.id.sync_button_sync_activity);
         syncButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // totalItems is 0, so nothing to sync
                 if (uploadIndex >= totalItems) {
 
                     Toast.makeText(SyncActivity.this, "There are no records to sync.", Toast.LENGTH_SHORT).show();
 
                 }
 
+                // Disable the sync button while the sync is in progress
                 syncButton.setEnabled(false);
+
                 try {
 
+                    // Make a call to upload the current record
                     Toast.makeText(SyncActivity.this, String.format("Uploading item %2d out of %2d", uploadIndex+1, totalItems), Toast.LENGTH_SHORT).show();
                     new MultipartUploadRequest(SyncActivity.this, ConstantsAndHelpers.UPLOAD_URL)
                         .addParameter("secret_key", ConstantsAndHelpers.APP_SECRET)
@@ -81,6 +108,7 @@ public class SyncActivity extends AppCompatActivity {
 
         super.onResume();
 
+        // Register a listener that is triggered when a sync operation completes
         uploadReceiver.register(SyncActivity.this);
 
     }
@@ -90,6 +118,7 @@ public class SyncActivity extends AppCompatActivity {
 
         super.onPause();
 
+        // Unregister the listener if this activity is going into the background
         uploadReceiver.unregister(SyncActivity.this);
 
     }
@@ -126,18 +155,23 @@ public class SyncActivity extends AppCompatActivity {
                                     int serverResponseCode,
                                     byte[] serverResponseBody) {
 
+                // Check if a response exists
                 if (serverResponseBody.length > 0) {
                     try {
 
-                        //Process JSON result
+                        // Process JSON result
                         JSONObject jObj = new JSONObject(new String(serverResponseBody));
 
-                        //Check if errors or success
+                        // Check if errors or success
                         if (jObj.isNull("errors")) {
 
+                            // If there are no errors, this means the record was uploaded successfully so the local copy is deleted
                             dataBaseHandler.removeRow(elementsToUpload.get(uploadIndex).getID());
+
+                            // Move to the next record to be uploaded
                             uploadIndex++;
 
+                            // Check if all records have been uploaded
                             if (uploadIndex >= totalItems) {
 
                                 Toast.makeText(SyncActivity.this, "All records have been uploaded.", Toast.LENGTH_SHORT).show();
@@ -146,6 +180,7 @@ public class SyncActivity extends AppCompatActivity {
 
                             }
 
+                            // Make a call to upload the next record
                             Toast.makeText(SyncActivity.this, String.format("Uploading item %2d out of %2d", uploadIndex+1, totalItems), Toast.LENGTH_SHORT).show();
                             new MultipartUploadRequest(SyncActivity.this, ConstantsAndHelpers.UPLOAD_URL)
                                 .addParameter("secret_key", ConstantsAndHelpers.APP_SECRET)
@@ -162,6 +197,8 @@ public class SyncActivity extends AppCompatActivity {
                                 .startUpload();
 
                         } else {
+
+                            // Display the error code sent by the server
                             JSONArray jArr = jObj.getJSONArray("errors");
                             for (int i = 0; i < jArr.length(); i++) {
                                 int errorCode = jArr.getJSONObject(i).getInt("error_code");
@@ -169,6 +206,7 @@ public class SyncActivity extends AppCompatActivity {
                                 Toast.makeText(SyncActivity.this, String.format("Error code %2d while uploading image.", errorCode), Toast.LENGTH_SHORT).show();
                                 syncButton.setEnabled(true);
                             }
+
                         }
                     } catch (Exception e) {
                         Toast.makeText(SyncActivity.this, "Unexpected error while uploading record. Please try again.", Toast.LENGTH_SHORT).show();
